5ff504f4765c1b5f6ae095693a5a0bfd5d627d72
bhatiaji112211 <102137520+bhatiaji112211@users.noreply.github.com>
2022-07-07 21:29:42 +0530

    new update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(".navbar-toggle").click(function(){
     $("ul").slideToggle();
 })
+
+
+function data(){
+    var userfName = document.getElementById("fname").value;
+    var userEmail = document.getElementById("email").value;
+    var useroption = document.getElementsByName("option")
+    var optn
+    for(let i=0;i<useroption.length;i++){
+        if(useroption[i].selected){
+            optn=useroption[i].value;
+        }
+    }
+    var userMsg = document.getElementById("cmt").value;
+
+    window.alert("\n"+userfName+"\n"+userEmail+"\n"+optn+"\n"+userMsg)
+
+}
